8f13fe8f70f46475a0b556e78bd60616ec2f4c0a
Tepbezdm <keremenko67@gmail.com>
2022-11-16 20:21:45 +0300

    Firts commit

---
+++ Vtitbid.ISP20.Eremenko.Price/Price.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtitbid.ISP20.Eremenko.Price
+{
+    internal class Price
+    {
+        public string Name;
+        public string Shop;
+        public string Cost;
+
+        public Price(string name, string shop, string cost)
+        {
+            Name = name;
+            Shop = shop;
+            Cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return $"Название товара: {Name}, название магазина: {Shop}, Стоимость товара (в рублях): {Cost}";
+        }
+    }
+}
---
+++ Vtitbid.ISP20.Eremenko.Price/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtitbid.ISP20.Eremenko.Price
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Price[] price = new Price[3];
+            for (int i=0; i< price.Length;i++)
+            {
+                Console.WriteLine("Название товара: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Название магазина: ");
+                string shop = Console.ReadLine();
+                Console.WriteLine("Стоимость товара (в рублях): ");
+                string cost = Console.ReadLine();
+                price[i] = new Price(name, shop, cost);
+            }
+            price = price.OrderBy(obj => obj.Shop).ToArray();
+            for (int i=0; i< price.Length;i++)
+            {
+                Console.WriteLine(price[i]);
+            }
+            Console.WriteLine("Поиск по магазину: ");
+            string ser = Console.ReadLine();
+            for (int i=0; i< price.Length; i++)
+            {
+                if (ser.ToLower() == price[i].Shop.ToLower())
+                {
+                    Console.WriteLine(price[i]);
+                }
+                else if (ser.ToLower() != price[i].Shop.ToLower() && i == price.Length -1)
+                {
+                    Console.WriteLine("Такого магазина нет!!!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
